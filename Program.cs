968700f80b2102ac82e2a8813d95a9530fbb972e
Shreesh Pandey <shvspandey@gmail.com>
2021-10-28 19:04:25 +0530

    Master branch displaying the welcome message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Program to find the Maximum value of the 3 using generics");
+        }
+    }
+}
