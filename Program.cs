be27a0c2a0e102846d22bec8ac93fd36f073efb2
Shreesh Pandey <shvspandey@gmail.com>
2021-10-29 16:03:22 +0530

    Generic Method T with Integer, Float, String Test Cases

--- Program.cs
+++ Program.cs
@@ -4,25 +4,27 @@ namespace Generics
 {
     class Program
     {
-        public static string maximumNumber(string String1, string String2, string String3)//string method which will return a string value
+        public static T maximumNumber<T>(T Value1, T Value2, T Value3) where T: IComparable//Generic Mathod T, IComparable is used as a generalized type-specific comparison method that a value type or class implements to order or sort its instances.
         {
-            if (String1.CompareTo(String2) > 0 && String1.CompareTo(String3) > 0 || String1.CompareTo(String2) >= 0 && String1.CompareTo(String3) > 0 || String1.CompareTo(String2) > 0 && String1.CompareTo(String3) >= 0)//if the value of the comparision between the instances created of string 1 and string 2 and string 1 and string 3 will be greater to or equal to 0.
+            if (Value1.CompareTo(Value2) > 0 && Value1.CompareTo(Value3) > 0 || Value1.CompareTo(Value2) >= 0 && Value1.CompareTo(Value3) > 0 || Value1.CompareTo(Value2) > 0 && Value1.CompareTo(Value3) >= 0)//if the value of the comparision between the instances created of Value 1 and Value 2 and Value 1 and Value 3 will be greater to or equal to 0.
             {
-                return String1;
+                return Value1;
             }
-            if (String2.CompareTo(String1) > 0 && String2.CompareTo(String3) > 0 || String2.CompareTo(String1) >= 0 && String2.CompareTo(String3) > 0 || String2.CompareTo(String1) > 0 && String2.CompareTo(String3) >= 0)//if the value of the comparision between the instances created of string 2 and string 1 and string 2 and string 3 will be greater to or equal to 0.
+            if (Value2.CompareTo(Value1) > 0 && Value2.CompareTo(Value3) > 0 || Value2.CompareTo(Value1) >= 0 && Value2.CompareTo(Value3) > 0 || Value2.CompareTo(Value1) > 0 && Value2.CompareTo(Value3) >= 0)//if the value of the comparision between the instances created of Value 2 and Value 1 and Value 2 and Value 3 will be greater to or equal to 0.
             {
-                return String2;
+                return Value2;
             }
-            if (String3.CompareTo(String1) > 0 && String3.CompareTo(String2) > 0 || String3.CompareTo(String1) >= 0 && String3.CompareTo(String2) > 0 || String3.CompareTo(String1) > 0 && String3.CompareTo(String2) >= 0)//if the value of the comparision between the instances created of string 3 and string 1 and string 3 and string 2 will be greater to or equal to 0.
+            if (Value3.CompareTo(Value1) > 0 && Value3.CompareTo(Value2) > 0 || Value3.CompareTo(Value1) >= 0 && Value3.CompareTo(Value2) > 0 || Value3.CompareTo(Value1) > 0 && Value3.CompareTo(Value2) >= 0)//if the value of the comparision between the instances created of Value 3 and Value 1 and Value 3 and Value 2 will be greater to or equal to 0.
             {
-                return String3;
+                return Value3;
             }
-            return String1;//will return any string.
+            return default;//will return a default value because it is generic and it can return any data type.
         }
-        static void Main(string[] args)
+        static void Main(string[] args)//main method
         {
-            Console.WriteLine("Maximum Number = " + maximumNumber("A","B" ,"C"));//Maximum String ASCII is at the Third Position.
+            Console.WriteLine("Maximum Number = " + Program.maximumNumber<int>(10, 8, 4));//Object created for a integer data type.
+            Console.WriteLine("Maximum Number = " + Program.maximumNumber<double>(10.8, 5.2, 4.8));//Object created for a double data type.
+            Console.WriteLine("Maximum Number = " + Program.maximumNumber<string>("C", "B", "A"));//Object created for a string data type.
         }
     }
 }
