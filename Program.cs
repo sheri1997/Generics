ed5ad615568b29392424abddf2a349510b130ed6
Shreesh Pandey <shvspandey@gmail.com>
2021-10-29 16:44:57 +0530

    Generic Class T with Integer, Double and String Data Types

--- Program.cs
+++ Program.cs
@@ -2,9 +2,16 @@
 
 namespace Generics
 {
-    class Program
+    public class Program<T>where T: IComparable//Generic Class, IComparable is used as a generalized type-specific comparison method that a value type or class implements to order or sort its instances.
     {
-        public static T maximumNumber<T>(T Value1, T Value2, T Value3) where T: IComparable//Generic Mathod T, IComparable is used as a generalized type-specific comparison method that a value type or class implements to order or sort its instances.
+        public T Value1, Value2, Value3;//Global Variables
+        public Program(T Value1, T Value2, T Value3)//Constructor used as reference for the global variables.
+        {
+            this.Value1 = Value1;
+            this.Value2 = Value2;
+            this.Value3 = Value3;
+        }
+        public static T maximumNumber(T Value1, T Value2, T Value3)//Generic Mathod T
         {
             if (Value1.CompareTo(Value2) > 0 && Value1.CompareTo(Value3) > 0 || Value1.CompareTo(Value2) >= 0 && Value1.CompareTo(Value3) > 0 || Value1.CompareTo(Value2) > 0 && Value1.CompareTo(Value3) >= 0)//if the value of the comparision between the instances created of Value 1 and Value 2 and Value 1 and Value 3 will be greater to or equal to 0.
             {
@@ -20,11 +27,14 @@ namespace Generics
             }
             return default;//will return a default value because it is generic and it can return any data type.
         }
-        static void Main(string[] args)//main method
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Maximum Number = " + Program.maximumNumber<int>(10, 8, 4));//Object created for a integer data type.
-            Console.WriteLine("Maximum Number = " + Program.maximumNumber<double>(10.8, 5.2, 4.8));//Object created for a double data type.
-            Console.WriteLine("Maximum Number = " + Program.maximumNumber<string>("C", "B", "A"));//Object created for a string data type.
+            Program<int> intMax = new Program<int>(10, 8, 5);//object creation for integer data type.
+            Program<double> doubleMax = new Program<double>(3.74, 5.94, 4.87);//object creation for the double data type.
+            Program<string> stringMax = new Program<string>("A", "B", "C");//object creation for the string data type.
+            Console.WriteLine("Maximum Integer Value = " + intMax);
+            Console.WriteLine("Maximum Double Value = " + doubleMax);
+            Console.WriteLine("Maximum String Value = " + stringMax);
         }
     }
 }
